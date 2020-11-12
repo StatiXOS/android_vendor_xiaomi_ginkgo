d21a9dbd8454930a5031bb49ec6980c3673287eb
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-11-12 21:04:19 +0530

    ginkgo: Update QTI telephony to LA.UM.9.11.r1-02100-NICOBAR.0
    Change-Id: I0d55e6d125808b177c69454fdc2cf44b81f4449a

--- ginkgo/ginkgo-vendor.mk
+++ ginkgo/ginkgo-vendor.mk
@@ -2316,4 +2316,5 @@ PRODUCT_PACKAGES += \
     WfdService \
     WfdCommon \
     com.qti.snapdragon.sdk.display \
-    vendor.qti.hardware.soter-V1.0-java
+    vendor.qti.hardware.soter-V1.0-java \
+    qti-telephony-common
